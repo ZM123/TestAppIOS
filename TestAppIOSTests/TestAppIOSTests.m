fdb82730f60c546411ba442f7ebb1b7d83e90126
Zal Machado <zalmachado@gmail.com>
2016-08-16 16:52:05 -0700

    fail test

--- TestAppIOSTests/TestAppIOSTests.m
+++ TestAppIOSTests/TestAppIOSTests.m
@@ -25,6 +25,7 @@
 }
 
 - (void)testExample {
+    XCTAssert(false);
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
